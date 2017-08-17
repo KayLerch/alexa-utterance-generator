bf5f6081c8b3622944daae3e19453e04b96b632d
kaylerch <dekstar@gmx.de>
2017-08-17 22:36:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Alexa Utterance Generator
+Use this tool if you'd like to generate hundreds and thousands of variant utterances
+for your Alexa skills from patterns.
+
+Create a new txt-file in _/src/main/resources/utterances_ and start writing down generic utterances.
+The magic comes with using placeholders in curly brackets. You need to set the chosen filename as a key
+in the UtteranceGenerator code-file or hand it in as a parameter when running this application from cli.
+
+## Value placeholders
+```xml
+help me {get|book} a {taxi|room|table}
+```
+
+results in six utterances (help me get a taxi, help me book a room etc.)
+
+## Resource placeholders
+
+If you have a lot of values for your placeholders it's worth maintaining them in
+a file. Create one to many new txt-files in _src/main/resources/placeholders_ and use
+the chosen file-name as the placeholder-name. If the reference cannot be resolved to
+a placeholder-file the script treats it as a value placeholder.
+
+Assume you created a file named "city.txt" that contains
+```xml
+Boston
+Berlin
+Bristol
+```
+
+an utterance like 
+
+```xml
+help me {get|book} a {taxi|room|table} in {city}
+```
+
+results in 18 variants (help me get a taxi in Boston, help me book a room in Berlin etc.)
+
+## Value ranges
+
+The generator is also able to resolve numeric range defined in placeholders.
+
+```xml
+help me {get|book} a {taxi|room|table} in {city} at {1-12}:00 {AM|PM}
+``` 
+
+results in 432 variants (help me get a taxi in Boston at 1:00 AM, help me book a table in Bristol at 12:00 PM etc.)
+
+## Escape placeholders
+
+Placeholders follow the same syntax as slots in regular utterances. You might want to keep a {slot} in 
+generated utterances without having it resolved by this script. 
+
+```xml
+help me {get|book} a {taxi|room|table} in {{city}} at {1-12}:00 {AM|PM}
+``` 
+
+returns utterances like "help me get a taxi in {city} at 1:00 AM".
---
+++ src/main/java/io/klerch/alexa/utterances/UtteranceGenerator.java
@@ -0,0 +1,136 @@
+package io.klerch.alexa.utterances;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UtteranceGenerator {
+    // Set this variable to the name of the file with utterances you created in the utterances-folder!
+    private final static String utteranceFileKey = "booking";
+
+    // stores the list of all generated utterances to avoid duplicates
+    private static List<String> utterances = new ArrayList<>();
+    // stores the list of values contained in placeholders of utterances
+    private static final Map<String, List<String>> placeholderValues = new HashMap<>();
+
+    public static void main(final String [] args) {
+        // get file-key from console
+        generateUtterances(Arrays.stream(args).findFirst().orElse(utteranceFileKey));
+    }
+
+    private static void generateUtterances(final String utteranceResourceId) {
+        getUtteranceList(utteranceResourceId).forEach(utterance -> {
+            final Map<String, String> placeholders = new HashMap<>();
+            final StringBuffer buffer = new StringBuffer();
+            // extract all the placeholders found in the utterance
+            final Matcher placeholdersInUtterance = Pattern.compile("\\{(.*?)\\}").matcher(utterance);
+            // for any of the placeholder ...
+            while (placeholdersInUtterance.find()) {
+                final String placeholderName = placeholdersInUtterance.group(1);
+                // generate unique placeholder
+                final String placeholderNameNew = "{" + UUID.randomUUID().toString() + "}";
+                placeholders.put(placeholderNameNew, placeholderName);
+                placeholdersInUtterance.appendReplacement(buffer, Matcher.quoteReplacement(placeholderNameNew));
+            }
+            placeholdersInUtterance.appendTail(buffer);
+
+            final String finalUtterance = buffer.toString();
+            generatePermutations(getPlaceholders(placeholders), 0, finalUtterance);
+        });
+    }
+
+    private static void generatePermutations(final List<Pair<String, List<String>>> lists, final int depth, final String utterance)
+    {
+        if(depth == lists.size()) {
+            printOut(utterance);
+            return;
+        }
+
+        for(int i = 0; i < lists.get(depth).getValue().size(); ++i) {
+            final Pair<String, List<String>> placeholder = lists.get(depth);
+            final String placeholderValue = placeholder.getValue().get(i);
+            final String placeholderName = placeholder.getKey();
+            generatePermutations(lists, depth + 1, utterance.replace(placeholderName, placeholderValue));
+        }
+    }
+
+    private static void printOut(final String utterance) {
+        final String normalizedUtterance = utterance.trim().replace("  ", " ");
+
+        if (!utterances.contains(normalizedUtterance)) {
+            utterances.add(normalizedUtterance);
+            System.out.println(normalizedUtterance);
+        }
+    }
+
+    private static List<Pair<String, List<String>>> getPlaceholders(final Map<String, String> placeholders) {
+        final List<Pair<String, List<String>>> placeholderWithValues = new ArrayList<>();
+
+        placeholders.forEach((placeholderName, placeholderOrigin) -> {
+            if (!placeholderValues.containsKey(placeholderName)) {
+                placeholderValues.put(placeholderName, getPlaceholderValueList(placeholderOrigin).orElse(resolvePlaceholderValue(placeholderOrigin)));
+            }
+            placeholderWithValues.add(new ImmutablePair<>(placeholderName, placeholderValues.get(placeholderName)));
+        });
+        return placeholderWithValues;
+    }
+
+    private static List<String> resolvePlaceholderValue(final String placeHolderName) {
+        final List<String> values = new ArrayList<>();
+
+        Arrays.stream(placeHolderName.split("\\|")).forEach(value -> {
+            final String[] ranges = value.split("-");
+            if (ranges.length == 2 && NumberUtils.isParsable(ranges[0]) && NumberUtils.isParsable(ranges[1])) {
+                final int r1 = Integer.parseInt(ranges[0]);
+                final int r2 = Integer.parseInt(ranges[1]);
+                final int min = Integer.min(r1, r2);
+                final int max = Integer.max(r1, r2);
+
+                for (Integer i = min; i <= max; i++) {
+                    values.add(i.toString());
+                }
+            } else {
+                values.add(value);
+            }
+        });
+        return values;
+    }
+
+    private static Optional<List<String>> getPlaceholderValueList(final String valueResource) {
+        final List<String> lines = getList(String.format("/placeholders/%s.txt", valueResource));
+        return lines.isEmpty() ? Optional.empty() : Optional.of(lines);
+    }
+
+    private static List<String> getUtteranceList(final String utteranceResource) {
+        return getList(String.format("/utterances/%s.txt", utteranceResource));
+    }
+
+    private static List<String> getList(final String fileName) {
+        final List<String> lines = new ArrayList<>();
+
+        final ClassLoader classLoader = UtteranceGenerator.class.getClassLoader();
+
+        Optional.ofNullable(UtteranceGenerator.class.getResource(fileName)).ifPresent(url -> {
+            final File file = new File(url.getFile());
+
+            try (final Scanner scanner = new Scanner(file)) {
+                while (scanner.hasNextLine()) {
+                    lines.add(scanner.nextLine());
+                }
+                scanner.close();
+            } catch (final IOException e) {
+                e.printStackTrace();
+            }
+        });
+        // eliminate empty lines
+        lines.removeIf(StringUtils::isBlank);
+        return lines;
+    }
+}
